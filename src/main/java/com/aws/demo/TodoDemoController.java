250aa93e9dc7ee3a561596a314e6044120d7dbd1
Guru <guru@gmail.com>
2019-08-06 21:57:15 +0530

    test5

--- src/main/java/com/aws/demo/TodoDemoController.java
+++ src/main/java/com/aws/demo/TodoDemoController.java
@@ -1,48 +1,16 @@
 package com.aws.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
-@Controller
+@RestController
 public class TodoDemoController {
-
-    @Autowired
-    private TodoItemRepository repository;
-
-    @RequestMapping("/")
-    public String index(Model model) {
-        //ArrayList<TodoItem> todoList = (ArrayList<TodoItem>) repository.findAll();
-        //model.addAttribute("items", todoList);
-        //model.addAttribute("newitem", new TodoItem());
-        //model.addAttribute("items", new TodoListViewModel(todoList));
-        return "index";
-    }
     
-    @RequestMapping("/hello")
-    public String hello(Model model) {
-        //ArrayList<TodoItem> todoList = (ArrayList<TodoItem>) repository.findAll();
-        //model.addAttribute("items", todoList);
-        //model.addAttribute("newitem", new TodoItem());
-        //model.addAttribute("items", new TodoListViewModel(todoList));
-        return "hello welcome!!";
+    @GetMapping("/welcome")
+    public String welcomePage() {
+        return "Welcome to EBS web app";
     }
 
-//     @RequestMapping("/add")
-//     public String addTodo(@ModelAttribute TodoItem requestItem) {
-//         TodoItem item = new TodoItem(requestItem.getTimeline(),requestItem.getType(),requestItem.getComplexity(), requestItem.getName());
-//         repository.save(item);
-//         return "redirect:/";
-//     }
-
-//     @RequestMapping("/update")
-//     public String updateTodo(@ModelAttribute TodoListViewModel requestItems,
-//     @RequestParam(value="action", required=false)String action){
-//         return "redirect:/";
-//     }
 }
--- src/main/java/com/aws/demo/TodoItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aws.demo;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TodoItemRepository extends CrudRepository<TodoItem, Long> {
-
-}
